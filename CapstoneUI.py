8019a6fcc98177ce80f738957f3131c316749e81
JianYiChong <chongjianyi6@gmail.com>
2024-10-17 09:08:25 +0000

    update

--- CapstoneUI.py
+++ CapstoneUI.py
@@ -21,7 +21,9 @@ selected = option_menu(
 # Home Section
 if selected == "Home":
     st.title("Welcome to the Home Page")
-    st.image("path/to/your/ITdevices.jpg", caption="Welcome Image", use_column_width=True)
+    st.image("ITdevices.jpg", caption="Welcome Image", use_column_width=True)
+
+
 
    
 
